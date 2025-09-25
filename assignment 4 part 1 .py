1a339279e6f3408ac640a213e1fbd90d61f40c9c
stpercell <stpercell@aggies.ncat.edu>
2025-09-25 15:32:44 -0400

    Add files via upload
    Pass Test Case 1: Initial game setup with required variables

---
+++ assignment 4 part 1 .py
@@ -0,0 +1,15 @@
+# part 1 code and variables
+student_name = "Shikel Percell"
+current_gpa = 3.5  # My irl stats I think
+study_hours = 25
+social_points = 50
+stress_level = 70
+
+print("=== Welcome to the Acedemic game with your host: Shikel ===")
+print(f"Student Name: {student_name}")
+print(f"Current GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}/100")
+
+
